bbd9585ff9106866061a17daf813d50d856650a0
YANGJOOHWA <zosel79764@gmail.com>
2020-09-17 17:29:14 +0900

    Initial commit

---
+++ mysite/hello/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def sayHello(request):
+    html  = """<title> 수업 </title>
+                <h1> Hello, !! </h1><br>
+               <h2> Hello, !! </h2><br>
+               <h3> Hello, !! </h3>
+               <a href="http://naver.com">네이버입니다. </a>
+               <p> 텍스트입니다. </p> 
+            """
+    return HttpResponse(html)
